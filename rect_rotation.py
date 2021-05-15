c05c2952782843bb2b042de09b53c497da0d1b69
NickMateus11 <mateus.nicholas@gmail.com>
2021-05-15 12:32:33 -0400

    3d cube rotation

--- rect_rotation.py
+++ rect_rotation.py
@@ -1,7 +1,7 @@
 import pygame
 
-import polygon
-from polygon import Rect, Prism
+import shapes
+from shapes import Rect, Prism, Cube
 
 
 w, h = 800, 600
@@ -15,26 +15,55 @@ BLACK = pygame.Color("black")
 WHITE = pygame.Color("white")
 
 
-polygon.init(screen)
+shapes.init(screen)
 
 
 def main():
 
-    rect_width = rect_height = 200
-    rect1 = Rect(w/2-rect_width/2, h/2-rect_height/2, rect_width, rect_height)
-    rect2 = Rect(w/2-rect_width/4, h/2-6*rect_height/10, rect_width, rect_height)
-    box = Prism(rect1,rect2)
+    # rect_width = rect_height = 200
+    # rect1 = Rect(w/2-rect_width/2, h/2-rect_height/2, rect_width, rect_height)
+    # rect2 = Rect(w/2-rect_width/4, h/2-6*rect_height/10, rect_width, rect_height)
+    # box = Prism(rect1,rect2)
+
+    s = 200
+    cube = Cube((w/2,h/2,0), s)
+    cube.rotate(0, 30, 60)
+
+    p = r = y = 0
+    max_rot = 1
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    if pygame.key.get_pressed()[pygame.K_RCTRL]:
+                        r += -1
+                    else:
+                        y += -1
+                if event.key == pygame.K_RIGHT:
+                    if pygame.key.get_pressed()[pygame.K_RCTRL]:
+                        r += 1
+                    else:
+                        y += 1
+                if event.key == pygame.K_UP:
+                    p += 1
+                if event.key == pygame.K_DOWN:
+                    p += -1
+
+        if abs(p) > max_rot:
+            p = max_rot if p>0 else -max_rot
+        if abs(r) > max_rot:
+            r = max_rot if r>0 else -max_rot
+        if abs(y) > max_rot:
+            y = max_rot if y>0 else -max_rot
 
         screen.fill(BLACK)
 
-        box.draw()
-        box.rotate(2)
+        cube.draw()
+        cube.rotate(p,r,y)
 
         pygame.display.flip()
         clock.tick(framerate)
---
+++ shapes.py
@@ -0,0 +1,107 @@
+import pygame
+import numpy as np
+
+screen = None
+BLACK = pygame.Color("black")
+WHITE = pygame.Color("white")
+
+def init(surface):
+    global screen
+    screen = surface
+
+
+class Cube():
+    def __init__(self, center_pos, s):
+        self.s = s
+        if len(center_pos) < 3:
+            center_pos = center_pos + (0,)
+        self.c = np.array(center_pos)
+        self.diag = np.sqrt(3*s**2)
+        self.points = [
+            (self.c + ( self.s/2,  self.s/2,  self.s/2)),
+            (self.c + ( self.s/2,  self.s/2, -self.s/2)),
+            (self.c + ( self.s/2, -self.s/2, -self.s/2)),
+            (self.c + ( self.s/2, -self.s/2,  self.s/2)),
+            (self.c + (-self.s/2,  self.s/2,  self.s/2)),
+            (self.c + (-self.s/2,  self.s/2, -self.s/2)),
+            (self.c + (-self.s/2, -self.s/2, -self.s/2)),
+            (self.c + (-self.s/2, -self.s/2,  self.s/2)),
+        ]
+
+    def draw(self):
+        projected_points = [p[:2] for p in self.points]
+        pygame.draw.lines(screen, WHITE, True, projected_points[:4])
+        pygame.draw.lines(screen, WHITE, True, projected_points[4:])
+        pygame.draw.lines(screen, WHITE, True, projected_points[:4:3] + projected_points[4::3][::-1])
+        pygame.draw.lines(screen, WHITE, True, projected_points[1:3] + projected_points[5:7][::-1])
+        for p in projected_points:
+            pygame.draw.circle(screen, WHITE, p, self.s//20)
+    
+    def rotate(self, pitch=0, roll=0, yaw=0):
+        if pitch:
+            theta = np.deg2rad(pitch)
+            r_mat = np.array([
+                [1, 0, 0], 
+                [0, np.cos(theta), -np.sin(theta)],
+                [0, np.sin(theta), np.cos(theta)]
+            ])
+            self.points = [self.c - np.matmul(r_mat, (self.c - p).T) for p in self.points]
+        if roll:
+            theta = np.deg2rad(roll)
+            r_mat = np.array([
+                [np.cos(theta), -np.sin(theta), 0], 
+                [np.sin(theta), np.cos(theta), 0],
+                [0, 0, 1]
+            ])
+            self.points = [self.c + np.matmul(r_mat, (self.c - p).T) for p in self.points]
+        if yaw:
+            theta = np.deg2rad(yaw)
+            r_mat = np.array([
+                [np.cos(theta), 0, np.sin(theta)], 
+                [0, 1, 0],
+                [-np.sin(theta), 0, np.cos(theta)]
+            ])
+            self.points = [self.c + np.matmul(r_mat, (self.c - p).T) for p in self.points]
+
+
+class Prism():
+    def __init__(self, f1, f2):
+        if not (len(f1.points) == len(f2.points)):
+            raise IndexError
+        self.face1 = f1
+        self.face2 = f2
+
+    def draw(self):
+        self.face2.draw()
+        self.face1.draw()
+        for i in range(len(self.face1.points)):
+            p1x,p1y = self.face1.points[i]
+            p2x,p2y = self.face2.points[i]
+            pygame.draw.line(screen, WHITE, (p1x,p1y), (p2x,p2y), width=2)
+
+    def rotate(self, deg):
+        self.face1.rotate(deg)
+        self.face2.rotate(deg)
+        
+
+class Rect():
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.centre = (x+w/2,y+h/2)
+        self.points = [(x,y), (x+w,y), (x+w,y+h), (x,y+h)]
+    
+    def draw(self):
+        pygame.draw.lines(screen, WHITE, True, self.points, width=2)
+        for p in self.points:
+            pygame.draw.circle(screen, WHITE, p, radius=min(self.w,self.h)//20)
+
+    def rotate(self, deg):
+        x0,y0 = self.centre
+        for i in range(len(self.points)):
+            x1,y1 = self.points[i]
+            r = np.sqrt((x1-x0)**2 + (y1-y0)**2)
+            phi = np.arctan2((y1-y0),(x1-x0)) + np.deg2rad(deg)
+            self.points[i] = (x0 + r*np.cos(phi), y0 + r*np.sin(phi))
